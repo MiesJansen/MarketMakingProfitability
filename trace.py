71b0cf52f08c6a06830ea09215c00def0eadd3e9
damianialex <alexdmail15@yahoo.com>
2016-02-27 03:34:47 -0500

    Created function Intital_deletes. It removes rows with empty cusip_id and only keeps rows whose trc_st value = T or R.

--- trace.py
+++ trace.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('C:\\Users\\Alex\\Desktop\\Industry Project\\trace_data.csv', sep=',')
+#page 12-13
+def Initial_deletes(df):
+	#delete items without cusip_id
+	df = df[df['cusip_id'] != '']
+	#delete cancellations(X) and corrections(C); 
+	#keep trade(T) and replacement/correction(R)
+	df = df[df['trc_st'].isin(["T", "R"])]
 
-for col in df.columns:
-	print (col)
+if __name__ == "__main__":
+	df = pd.read_csv('C:\\Users\\Alex\\Desktop\\Industry Project\\trace_data.csv', sep=',')
+	for col in df.columns:
+		print (col)
+
+	Initial_deletes(df)
