7adf5d4904f463e95fb01fc1ab095dec30a88237
damianialex <alexdmail15@yahoo.com>
2016-02-28 23:56:07 -0500

    Added post 2012 functionality. Uncomment the print statements to verify functionality works. Combined with pre2012 code into new file pre_and_post_2012.py

---
+++ pre_and_post_2012.py
@@ -0,0 +1,161 @@
+import pandas as pd
+import numpy as np
+
+# TO-DO: pre/post2012 code needs to be merge (now standalone).
+# TO-DO: add some statistics, e.g. # of matched same day cancellation/correction
+#        or even keep a copy of all deleted entries for record.
+
+# enhanced TRACE columns that are not useful in analysis
+COLS_TO_REMOVE = ['company_symbol', 'sale_cndtn2_cd', 'dissem_fl', 'SUB_PRDCT',\
+                  'STLMNT_DT', 'TRD_MOD_3', 'TRD_MOD_4', 'LCKD_IN_IND', \
+                  'PR_TRD_DT']
+
+# dropping rows and columns, applied to all data
+def Initial_deletes(df):    
+    # delete items without cusip_id
+    df = df[df['cusip_id'] != '']
+    
+    # find common elements in to_drop columns and imported dataframe columns
+    cols_to_remove = [col for col in df.columns if col in COLS_TO_REMOVE]
+    df.drop(cols_to_remove, axis = 1,inplace = True)
+
+    return df
+
+    
+# ASSUMPTIONS: each input dataframe should have identical cusip_id
+# Processing applied only to pre-2012 data
+def pre2012(df):
+    df = sameday_corrections(df)
+    df = reversals(df)
+    
+    return df
+
+
+# Adjust trade cancellations and corrections within the same day
+def sameday_corrections(df):
+    # Delete the following:
+    # 1. cancellation trades themselves
+    trds_to_drop = df[df['trc_st'] == "C"]
+    
+    # TO-FIX: this loop seems super inefficient (very slow).
+    #         How to isolate trades of the same day better?
+    # 2. original trade of cancellation(C) and correction(W) orders 
+
+    tradesCW = df[df['trc_st'].isin(["C", "W"])]
+    print (tradesCW['trd_rpt_dt'])
+    for idx, trd in tradesCW.iterrows():
+        # Filtre out sameday trades
+        day_trds = df[df['trd_rpt_dt'] == trd['trd_rpt_dt']]
+        
+        # Find matching message sequence number
+        orig_trds = day_trds[day_trds['msg_seq_nb'] == trd['orig_msg_seq_nb']]
+        trds_to_drop = trds_to_drop.append(orig_trds)
+    
+    df = df.drop(trds_to_drop.index, axis = 0)
+    
+    return df
+
+def reversals(df):
+    # Identify reversals
+    df_R = df[df['asof_cd'] == "R"]
+    # Also, Reversals must have report day > execution date
+    df_R = df_R[df_R['trd_rpt_dt'] > df_R['trd_exctn_dt']]
+    
+    # All reversals will be deleted
+    trds_to_drop = df_R
+    
+    nRev = trds_to_drop.shape[0]
+    print "Number of Reversals is", nRev
+    
+    for idx, trd in df_R.iterrows():
+        # Filtre out trades with same execution date
+        day_trds = df[df['trd_exctn_dt'] == trd['trd_exctn_dt']]
+        # Avoid matching Reversals with itself
+        day_trds = day_trds[day_trds['asof_cd'] != "R"]
+        
+        # Find trades that match with the Reversal trade
+        match = np.all([day_trds['trd_exctn_tm'] == trd['trd_exctn_tm'], \
+                       day_trds['rptd_pr'] == trd['rptd_pr'], \
+                       day_trds['entrd_vol_qt'] == trd['entrd_vol_qt'], \
+                       day_trds['rpt_side_cd'] == trd['rpt_side_cd'], \
+                       day_trds['cntra_mp_id'] == trd['cntra_mp_id']], \
+                       axis = 0)
+        Rmatch = day_trds[match]
+        
+        if Rmatch.shape[0] > 0:
+            # Only use at most 1 match of reversal
+            trds_to_drop = trds_to_drop.append(Rmatch.iloc[0])
+    
+    nMatch = trds_to_drop.shape[0] - nRev
+    print "Number of Reverasal match is", nMatch
+    
+    df = df.drop(trds_to_drop.index, axis = 0)
+    
+    return df
+
+def post2012(df):
+    df = sameday_corrections_post(df)
+    return df
+
+def sameday_corrections_post(df):
+    # Delete the following:
+
+    # 1. cancellations and corrections
+    df_temp_deleteI_NEW = df[df['trc_st'].isin(["X", "C"])]
+
+    # 2. Reversals
+    df_temp_deleteII_NEW = df[df['trc_st'] == "Y"]
+
+    # 3. Raw data with cancellations, reversals, and corrections removed
+    df_temp_raw = df[~df['trc_st'].isin(["X", "C", "Y"])]
+
+    merge_columns = ['cusip_id', 'entrd_vol_qt', 'rptd_pr',
+        'trd_exctn_dt', 'trd_exctn_tm', 'rpt_side_cd', 'cntra_mp_id', 
+        'msg_seq_nb']
+
+    # inner merge temp_raw and temp_deleteI_NEW on merge columns
+    # keep index of temp_deleteI_NEW (same as original parameter df)
+    df_temp_raw2_del_rows = pd.merge(df_temp_raw[list(merge_columns)], 
+        df_temp_deleteI_NEW[list(merge_columns)],
+        how = 'inner', on=merge_columns, right_index=True)
+
+    #---- Uncomment print statements to verify right number of columns removed
+    # at each step. ------------
+
+    #print('df_temp_raw shape: ', df_temp_raw.shape)
+    #print('df_temp_raw2_del_rows shape: ', df_temp_raw2_del_rows.shape)
+
+    # Drop merged columns from temp_raw and temp_deleteI_NEW
+    df_temp_raw2 = df_temp_raw.drop(df_temp_raw2_del_rows.index, axis=0)
+
+    #print('df_temp_raw_2 shape: ', df_temp_raw2.shape)
+
+    df_temp_raw3_del_rows = pd.merge(df_temp_raw2[list(merge_columns)], 
+        df_temp_deleteII_NEW[list(merge_columns)],
+        how = 'inner', on=merge_columns, right_index=True)
+
+    #print('df_temp_raw3_del_rows shape: ', df_temp_raw3_del_rows.shape)
+
+    df_temp_raw3 = df_temp_raw2.drop(df_temp_raw3_del_rows.index, axis=0)  
+
+    #print('df_temp_raw3 shape: ', df_temp_raw3.shape)  
+
+    return df_temp_raw3
+
+## For testing only
+if __name__ == "__main__":
+    df = pd.read_csv('C:\Users\Alex\Desktop\Industry Project\\trace_data.csv', \
+                     low_memory=False) # To silent warnings
+
+    # common pre-processing for all data
+    df = Initial_deletes(df)
+    
+    # Extract data before Feb 6, 2012
+    df_pre2012 = df[df['trd_rpt_dt'] < 20120206]
+    df_post2012 = df[df['trd_rpt_dt'] >= 20120206]
+
+    df_pre2012 = pre2012(df_pre2012)
+    df_post2012 = post2012(df_post2012)
+    
+    print(df_pre2012.shape)
+    print(df_post2012.shape)
