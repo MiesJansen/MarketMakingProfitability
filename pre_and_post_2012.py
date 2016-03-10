893b96769ef6e86443ed3c67097cb2a081ef5494
damianialex <alexdmail15@yahoo.com>
2016-03-09 20:47:57 -0800

    Separated dataframe by month. Performed regression on each month. Printed regression results from each month.

--- pre_and_post_2012.py
+++ pre_and_post_2012.py
@@ -241,26 +241,30 @@ def Calculate_First_Proxy(df):
     df['volume_and_sign'] = df['volume_and_sign'] * df['entrd_vol_qt']
 
     # convert int64 date series to DateTime series
-    df['trd_exctn_dt'] = pd.to_datetime(df['trd_exctn_dt'], unit='s')
+    df['trd_exctn_dt'] = pd.to_datetime(df['trd_exctn_dt'], format='%Y%m%d')
     # set DateTime series as index od dataframe
     df = df.set_index('trd_exctn_dt')
+       
+    res_summary_list = []   #stores regression outputs
 
     # Group dataframe on index by month and year
-    '''    
-    for df_group in df.groupby(pd.TimeGrouper("M")):
+    for date, df_group in df.groupby(pd.TimeGrouper("M")):
+        #run linear regression using equation (2) from pdf 
         y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', data=df_group, return_type='dataframe')
-    '''
+        mod = sm.OLS(y,X)
+        res = mod.fit()
+        #store regression for ouput for each month
+        res_summary_list.append([date, res])
+
+    #print regression output summary for each month
+    for date, res in res_summary_list:
+        print date.month, '-', date.year
+        print res.params, '\n'
+        #print (res.summary())
 
     #df.to_csv('output.csv')
 
     #run linear regression using equation (2) from pdf 
-    y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', data=df, return_type='dataframe')
-
-    mod = sm.OLS(y,X)
-
-    res = mod.fit()
-
-    print res.summary()
 
 
 ## For testing only
