7e7bb5cd63848ce6a63fd36af50cd3c2f511fefa
damianialex <alexdmail15@yahoo.com>
2016-03-10 17:40:27 -0800

    Modified main.py and liquidity_proxy.py to take in multiple bonds.

--- liquidity_proxy.py
+++ liquidity_proxy.py
@@ -5,12 +5,46 @@ from patsy import dmatrices
 import matplotlib
 import matplotlib.pyplot as plt
 
-def Calculate_First_Proxy(df):
-    #price = rptd_pr
-    #yield = yld_pt
-    #volume = entrd_vol_qt
-    #yield sign = yld_sign_cd
-    #date = trd_exctn_dt
+def Calculate_First_Proxy(df_list):     
+    for idx, df in enumerate(df_list):
+        df_list[idx] = Modify_Columns(df)
+
+    #res_array = []
+    #res_array_list = [[]]   #stores regression outputs
+    date_array = []
+    date_array_list = [[]]         #stores datetime objects
+    liquidity_array = []
+    liquidity_array_list = [[]]
+
+    # Group dataframe on index by month and year
+    for df in df_list: 
+        for date, df_group in df.groupby(pd.TimeGrouper("M")):
+            #run linear regression using equation (2) from pdf 
+            y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', 
+                data=df_group, return_type='dataframe')
+            mod = sm.OLS(y,X)
+            res = mod.fit()
+            #store regression for ouput for each month
+            #res_array.append([date, res])
+            date_array.append(date)
+            liquidity_array.append(res.params[2])
+
+        #res_array_list.append(res_array)
+        date_array_list.append(date_array)
+        liquidity_array_list.append(liquidity_array)
+
+        #res_array.clear()
+        date_array = []
+        liquidity_array = []
+    
+    # plot liquidity vs date for each individual bond
+    for date_list, liquidity_list in zip(date_array_list, liquidity_array_list):
+        plt.plot(date_list, liquidity_list)
+        plt.show()
+
+def Modify_Columns(df):
+    #price = rptd_pr | yield = yld_pt | volume = entrd_vol_qt
+    #yield sign = yld_sign_cd | date = trd_exctn_dt
 
     df['rptd_pr_1'] = df['rptd_pr'].shift(-1)
     df['yld_pt_1'] = df['yld_pt'].shift(-1)
@@ -24,28 +58,5 @@ def Calculate_First_Proxy(df):
     df['trd_exctn_dt'] = pd.to_datetime(df['trd_exctn_dt'], format='%Y%m%d')
     # set DateTime series as index od dataframe
     df = df.set_index('trd_exctn_dt')
-       
-    res_summary_list = []   #stores regression outputs
-    list_of_dates = []         #stores datetime objects
-    liquidity_factor_list = []
 
-    # Group dataframe on index by month and year
-    for date, df_group in df.groupby(pd.TimeGrouper("M")):
-        #run linear regression using equation (2) from pdf 
-        y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', data=df_group, return_type='dataframe')
-        mod = sm.OLS(y,X)
-        res = mod.fit()
-        #store regression for ouput for each month
-        res_summary_list.append([date, res])
-        list_of_dates.append(date)
-        liquidity_factor_list.append(res.params[2])
-
-    #print regression output summary for each month
-    '''for date, res in res_summary_list:
-        print date
-        print res.params[2]
-        #print (res.summary())'''
-
-    #dates = matplotlib.dates.date2num(list_of_dates)
-    plt.plot(list_of_dates, liquidity_factor_list)
-    plt.show()
+    return df
--- main.py
+++ main.py
@@ -13,19 +13,23 @@ DATA_NAME = 'BAC.HQO_20100331_20140330'
 
 ## For testing only
 if __name__ == "__main__":
-    df0 = pd.read_csv(DATA_PATH + DATA_NAME + "_raw.csv", \
-                     low_memory=False) # To silent warnings
+    '''df0 = pd.read_csv(DATA_PATH + DATA_NAME + "_raw.csv", \
+                     low_memory=False) # To silent warnings'''
 
-    '''df0 = pd.read_csv('C:\Users\Alex\Desktop\Industry_Project\\trace_data.csv')'''
+    df0 = pd.read_csv('C:\Users\Alex\Desktop\Industry_Project\\BAC.HQO_CHK.HE_raw.csv')
 
     # .csv file downloaded from TRACE should already sort by bond symbol
     #  and each bond symbol corresponds to an unique cusip_id
     cusip_id_list = df0['cusip_id'].unique().tolist()
+
+    df_list = []
     for cusip_id in cusip_id_list:
         #print "Cleaning cusip_id", cusip_id
         
         # dataframe holding all trades of ONE bond at a time
         df1 = df0.loc[df0['cusip_id'] == cusip_id]
         df2 = cln.clean_bond(df1, DATA_PATH)
+
+        df_list.append(df2)
         
-        lpx.Calculate_First_Proxy(df2)
+    lpx.Calculate_First_Proxy(df_list)
