c826498e1dc0b3c677d18f1b20b482fe290859c5
damianialex <alexdmail15@yahoo.com>
2016-03-10 16:38:56 -0800

    Graphing for proxy.

--- liquidity_proxy.py
+++ liquidity_proxy.py
@@ -24,23 +24,26 @@ def Calculate_First_Proxy(df):
     df = df.set_index('trd_exctn_dt')
        
     res_summary_list = []   #stores regression outputs
+    list_of_dates = []         #stores datetime objects
+    liquidity_factor_list = []
 
     # Group dataframe on index by month and year
     for date, df_group in df.groupby(pd.TimeGrouper("M")):
         #run linear regression using equation (2) from pdf 
-        y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', data=df_group, \
-                        return_type='dataframe')
+        y,X = dmatrices('rptd_pr_1 ~ rptd_pr + volume_and_sign', data=df_group, return_type='dataframe')
         mod = sm.OLS(y,X)
         res = mod.fit()
         #store regression for ouput for each month
         res_summary_list.append([date, res])
+        list_of_dates.append(date)
+        liquidity_factor_list.append(res.params[2])
 
     #print regression output summary for each month
-    for date, res in res_summary_list:
-        print date.month, '-', date.year
-        print res.params, '\n'
-        #print (res.summary())
+    '''for date, res in res_summary_list:
+        print date
+        print res.params[2]
+        #print (res.summary())'''
 
-    #df.to_csv('output.csv')
-
-    #run linear regression using equation (2) from pdf 
+    #dates = matplotlib.dates.date2num(list_of_dates)
+    plt.plot(list_of_dates, liquidity_factor_list)
+    plt.show()
