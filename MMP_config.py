faff9dab984cabac2f4ed4b86ffe7cef13560e93
Roklion <yaodongy1018@gmail.com>
2016-03-13 20:09:36 -0700

    Modify file reading machnism to deal with large raw data files

--- MMP_config.py
+++ MMP_config.py
@@ -3,9 +3,13 @@
 # A directory for holding raw/processed data
 # DO NOT push any data file to Github
 DATA_PATH = './data/'
+OUTPUT_PATH = './data/output/'
 
 # Name of the datafile
-DATA_NAME = 'BAC.HQO_20100331_20140330_raw'
+DATA_NAME = 'List0'
+
+# Indicator of whether output individual clean bond data to .csv
+doOutput = True
 
 # Number of subsections dataframe (because TRACE encourage to keep data
 #  smaller than 2GB)
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import MMP_config as cfg
 
 ## For testing only
 if __name__ == "__main__":
-    '''df0 = pd.read_csv(cfg.DATA_PATH + cfg.DATA_NAME + ".csv", \
-                     low_memory=False) # To silent warnings'''
-
-    df0 = pd.read_csv('C:\Users\Alex\Desktop\Industry_Project\\BAC.HQO_CHK.HE_raw.csv')
+    # Due to large size of raw TRACE data, read by chunks and concat into one
+    #  dataset
+    df_chunks = pd.read_csv(cfg.DATA_PATH + cfg.DATA_NAME + ".csv", chunksize = 100000,\
+                            low_memory=False) # To silent warnings
+    df0 = pd.concat(df_chunks, ignore_index = True)
 
     # .csv file downloaded from TRACE should already sort by bond symbol
     #  and each bond symbol corresponds to an unique cusip_id
@@ -24,8 +25,9 @@ if __name__ == "__main__":
         # dataframe holding all trades of ONE bond at a time
         df1 = df0.loc[df0['cusip_id'] == cusip_id]
 
-        df2 = cln.clean_bond(df1, cfg.DATA_PATH)
+        df2 = cln.clean_bond(df1, cfg.DATA_PATH, cfg.doOutput)
         df_list.append(df2)
         
     df1 = lpx.Calculate_First_Proxy(df_list)
     lpx.Plot_Liquidity(df1, 'residual_term')
+    
