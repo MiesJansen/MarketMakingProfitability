df38813745cc56e76043656036cc9713d6636a60
Roklion <yaodongy1018@gmail.com>
2016-03-15 15:54:21 -0700

    Non functional change to config file

--- MMP_config.py
+++ MMP_config.py
@@ -1,5 +1,7 @@
 # Some universal constants for the entire project
 
+# PreProcessing/Cleaning data
+#---------------------------------------------
 # A directory for holding raw/processed data
 # DO NOT push any data file to Github
 DATA_PATH = './data/'
@@ -8,17 +10,22 @@ OUTPUT_PATH = './data/output/'
 # Whether to process raw data or read existing clean data directly
 readRaw = False
 
-# Name of the datafile
-RAW_DATA_NAMES = ['List0', 'List1', 'List2', 'List3', 'List4']
+# Only use when readRaw = False
+doOutput = False    # Whether output individual clean bond data to .csv
 CLEAN_DATA_FILE = 'list_clean'
 
+# Only use when readRaw = True
+# Name of the datafile
+RAW_DATA_NAMES = ['List0', 'List1', 'List2', 'List3', 'List4']
+
+# Calculating proxies
+#-----------------------------------------------
 # Start and end day of data
 dt_start = '20100401'
 dt_end = '20140331'
 
-# Indicator of whether output individual clean bond data to .csv
-doOutput = False
-
+# Generating list of cusip id's
+#------------------------------------------------
 # Number of subsections dataframe (because TRACE encourage to keep data
 #  smaller than 2GB)
 NUM_SUB_DF = 5
