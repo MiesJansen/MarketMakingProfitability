35fa268b766bc4ddf41adce1625847494d8cfa2e
damiani_alex <alexdmail15@yahoo.com>
2016-03-24 20:16:25 -0700

    Get r2 adjusted for each bond within constraint.

--- Fama_French.py
+++ Fama_French.py
@@ -40,45 +40,53 @@ def FamaFrenchReg(df_list, df_liq):
     #---------------------------------------------
     df_yld_list = []
     for df in df_list:
-	df_list_monthly = []
-	for date, df_group in df.groupby(pd.TimeGrouper("M")):
-	    df_month = pd.DataFrame(df_group)
-	    if df_month.empty:
-		continue
-	    
-	    # Take END OF THE MONTH yield as monthly yield, other possible
-	    #  monthly yield is AVERAGE, or BEGINNING OF THE MONTH
-	    df_month_yld = df_month.tail(1)
-	    df_list_monthly.append(df_month_yld)
-	
-	# Combine monthly data with Fama French parameters,
-	#  and add to a list for every bond
-	df_month_yld = pd.concat(df_list_monthly)
-	df_month_yld['trd_exctn_dt'] = \
-            df_month_yld['trd_exctn_dt'].astype(str).str[0:6]	
-	df_month_yld = pd.merge(left = df_month_yld, right = df_ff_params,
-	                        left_on = ['trd_exctn_dt'], right_on = ['Date'],
-	                        how = 'inner')
-	if df_month_yld.shape[0] > 0:
-	    df_yld_list.append(df_month_yld)
+        df_list_monthly = []
+        for date, df_group in df.groupby(pd.TimeGrouper("M")):
+            df_month = pd.DataFrame(df_group)
+            if df_month.empty:
+                continue
+            
+            # Take END OF THE MONTH yield as monthly yield, other possible
+            #  monthly yield is AVERAGE, or BEGINNING OF THE MONTH
+            df_month_yld = df_month.tail(1)
+            df_list_monthly.append(df_month_yld)
+        
+        # Combine monthly data with Fama French parameters,
+        #  and add to a list for every bond
+        df_month_yld = pd.concat(df_list_monthly)
+        df_month_yld['trd_exctn_dt'] = \
+                df_month_yld['trd_exctn_dt'].astype(str).str[0:6]   
+        df_month_yld = pd.merge(left = df_month_yld, right = df_ff_params,
+                                left_on = ['trd_exctn_dt'], right_on = ['Date'],
+                                how = 'inner')
+        if df_month_yld.shape[0] > 0:
+            df_yld_list.append(df_month_yld)
     
     # Run linear regression using equation (9) from pdf for each bond
     #-----------------------------------------------------
     df_list_Beta = []
+    # list of adjusted r2 values for all bonds
+    adj_rsqaured_list = []
     headers = ['cusip_id', 'intercept', 'MKT', 'SMB', 'HML', 'DEF', 'TERM', 'L']
     for df in df_yld_list:
-	df['e_return'] = df['yld_pt'] - df['RF']
-	y,X = dmatrices('e_return ~ MKT + SMB + HML + DEF + TERM + L',\
-	                data = df, return_type = 'dataframe')
-	mod = sm.OLS(y,X)
-	res = mod.fit()
-	
-	# Add cusip id and all regression results to a list
-	beta_list = [df.get_value(df.index[0], 'cusip_id')]
-	beta_list += list(res.params[:7])
-	
-	df_list_Beta.append(beta_list)
-	
+        df['e_return'] = df['yld_pt'] - df['RF']
+        y,X = dmatrices('e_return ~ MKT + SMB + HML + DEF + TERM + L',\
+                        data = df, return_type = 'dataframe')
+        mod = sm.OLS(y,X)
+        res = mod.fit()
+
+        # trades over >= 10 months for meaningful r2 value
+        if df.shape[0] >= 10:
+            #print df['cusip_id'][0], res.rsquared, res.rsquared_adj
+            adj_rsqaured_list.append(res.rsquared_adj)
+        
+        # Add cusip id and all regression results to a list
+        beta_list = [df.get_value(df.index[0], 'cusip_id')]
+        beta_list += list(res.params[:7])
+        
+        df_list_Beta.append(beta_list)
+    
     df_Beta = pd.DataFrame(df_list_Beta, columns = headers)
     df_Beta.to_csv(cfg.DATA_PATH + cfg.CLEAN_DATA_FILE + "_Betas.csv",
                    index = False)
+
